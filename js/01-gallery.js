a62539ceea98708ad04d9dca0e9b14de3d75816b
kaktuz <kaktuz911@gmail.com>
2023-04-25 22:59:36 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-
 const galleryRef = document.querySelector('.gallery');
 const galleryCard = createGalleryCard(galleryItems);
 
